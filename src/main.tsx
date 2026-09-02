c1e00a16e981d615724a62fe8ed2220f4b5d7be1
Lucy Mazzini Lessa <labyrtcontact@gmail.com>
2026-09-02 22:04:32 +0000

    Load final visual QA overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
 import './styles/pixel-perfect.scss';
+import './styles/final-audit.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
