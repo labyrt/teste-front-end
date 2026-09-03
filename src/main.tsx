cdda8e1e79c22ed90b65717ebeb40da77e46724f
Lucy Mazzini Lessa <labyrtcontact@gmail.com>
2026-09-03 03:03:29 +0000

    Use a single organized stylesheet entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/main.scss';
-import './styles/pixel-perfect.scss';
-import './styles/final-audit.scss';
-import './styles/runtime-fidelity.scss';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
